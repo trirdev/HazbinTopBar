dcc57070cc5e7c15e1c8386033227a45c8205032
trirdev <100738139+trirdev@users.noreply.github.com>
2022-04-23 16:03:32 -0700

    Update hazbin.lua

--- hazbin.lua
+++ hazbin.lua
@@ -1,3 +1,17 @@
 -- Within a LocalScript in StarterPlayerScripts and assuming TopbarPlus is placed in ReplicatedStorage
 local Icon = require(game:GetService("ReplicatedStorage").Icon)
 local icon = Icon.new()
+icon:setLabel("Hazbin Stuff")
+
+icon:bindToggleItem(Sus)
+icon.deselected:Connect(function()
+    Sus.Visible = false
+end)
+icon.selected:Connect(function()
+    Sus.Visible = true
+end)
+
+icon:setTip("Open Condo Stuff (E)")
+icon:bindToggleKey(Enum.KeyCode.E)
+icon:setCornerRadius(0, 8)
+icon:SetMid()

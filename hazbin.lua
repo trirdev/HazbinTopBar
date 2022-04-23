a3723cd5fe1ff1b1665e83eff90ffe7bd9c0d9ee
trirdev <100738139+trirdev@users.noreply.github.com>
2022-04-23 15:41:34 -0700

    Create hazbin.lua

---
+++ hazbin.lua
@@ -0,0 +1,3 @@
+-- Within a LocalScript in StarterPlayerScripts and assuming TopbarPlus is placed in ReplicatedStorage
+local Icon = require(game:GetService("ReplicatedStorage").Icon)
+local icon = Icon.new()
